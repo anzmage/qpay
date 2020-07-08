a8f44eba13ca4a219289c74818261973e2b1e89b
Ansyori <ansyori@gmail.com>
2020-07-08 16:23:50 +0000

    popup pay

--- view/frontend/web/js/view/payment/method-renderer/mpgs-method.js
+++ view/frontend/web/js/view/payment/method-renderer/mpgs-method.js
@@ -5,9 +5,10 @@ define(
         'ko',
         'jquery',
         'Magento_Checkout/js/view/payment/default',
-		'Nology_Qpay/js/action/set-payment-method-action'
+		'Nology_Qpay/js/action/set-payment-method-action',
+		'https://dohabank.gateway.mastercard.com/checkout/version/56/checkout.js'
     ],
-    function (ko, $, Component, setPaymentMethodAction) {
+    function (ko, $, Component, setPaymentMethodAction, dohabank) {
         'use strict';
         return Component.extend({
             defaults: {
@@ -17,6 +18,56 @@ define(
             getMailingAddress: function () {
                 return window.checkoutConfig.payment.checkmo.mailingAddress;
             },
+			popupPayment: function(){
+				
+				console.log('trigger pop here');
+				
+				Checkout.configure({
+					merchant   : 'DB92668',
+					order      : {
+						amount     : function () { //Dynamic calculation of amount
+							return 80 + 20
+						},
+						currency   : 'QAR',
+						description: 'Ordered goods',
+						id: '0000002'
+					},
+					billing    : {
+						address: {
+							street       : '123 Customer Street',
+							city         : 'Metropolis',
+							postcodeZip  : '99999',
+							stateProvince: 'QTAR',
+							country      : 'QAT'
+						}
+					},
+					interaction: {
+						operation: 'PURCHASE',
+						merchant      : {
+							name   : 'Nama Merchant Euy',
+							address: {
+										  line1: '200 Sample St',
+										  line2: '1234 Example Town'            
+							},
+							email  : 'order@yourMerchantEmailAddress.com',
+							phone  : '+1 123 456 789 012'
+						},
+						locale        : 'en_US',
+						theme         : 'default',
+						displayControl: {
+							billingAddress  : 'OPTIONAL',
+							customerEmail   : 'OPTIONAL',
+							orderSummary    : 'SHOW',
+							shipping        : 'HIDE'
+						}
+					}
+				});
+				
+				Checkout.showLightbox();
+				
+				return false;
+				
+			},
             getInstructions: function () {
 				//var instructions = window.checkoutConfig.payment.instructions[this.item.method];
 				return '';
